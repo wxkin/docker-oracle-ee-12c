d67a34ced1de439654c782e6480b25838b1d72da
wxkin <kin@wna.gr>
2018-07-09 12:01:23 +0200

    Handle MongoDB installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,6 +60,15 @@ printf '
 ' >> /etc/yum.repos.d/mongodb-org-4.0.repo
 }
 
+installMongo(){
+	sudo yum install -y mongodb-org-4.0.0 mongodb-org-server-4.0.0 mongodb-org-shell-4.0.0 mongodb-org-mongos-4.0.0 mongodb-org-tools-4.0.0
+	# Because of SELinux enable access to port.
+	semanage port -a -t mongod_port_t -p tcp 27017
+	echo "Starting MongoDB service"
+	sudo service mongod start
+}
+
+
 case "$1" in
 	'')
 		startNoVNC
@@ -153,4 +162,6 @@ case "$1" in
 esac
 
 addMongoRepository()
-sudo yum install -y mongodb-org-4.0.0 mongodb-org-server-4.0.0 mongodb-org-shell-4.0.0 mongodb-org-mongos-4.0.0 mongodb-org-tools-4.0.0
+installMongo()
+
+

39258d46750da82985d71af16d93a60f05de2b2e
wxkin <kin@wna.gr>
2018-07-09 11:56:13 +0200

    Add MongoDB CentOS Repository and install Mongo 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,6 +48,18 @@ startNoVNC () {
 	sleep 3
 }
 
+addMongoRepository(){
+printf '
+	[mongodb-org-4.0]
+	name=MongoDB Repository
+	baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
+	gpgcheck=1
+	enabled=1
+	gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
+
+' >> /etc/yum.repos.d/mongodb-org-4.0.repo
+}
+
 case "$1" in
 	'')
 		startNoVNC
@@ -139,3 +151,6 @@ case "$1" in
 		exec "$@"
 		;;
 esac
+
+addMongoRepository()
+sudo yum install -y mongodb-org-4.0.0 mongodb-org-server-4.0.0 mongodb-org-shell-4.0.0 mongodb-org-mongos-4.0.0 mongodb-org-tools-4.0.0

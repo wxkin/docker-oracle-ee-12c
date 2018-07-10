1e93fd20165f2243d132580c3ba64d81d23de4c1
Nikolaos Bouras <nikolaos.bouras@asml.com>
2018-07-10 08:10:05 +0200

    Remove empty spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,13 +50,12 @@ startNoVNC () {
 
 addMongoRepository(){
 printf '
-	[mongodb-org-4.0]
-	name=MongoDB Repository
-	baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
-	gpgcheck=1
-	enabled=1
-	gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
-
+[mongodb-org-4.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
 ' >> /etc/yum.repos.d/mongodb-org-4.0.repo
 }
 
